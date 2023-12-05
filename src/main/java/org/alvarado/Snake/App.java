74c5a7ac88cd5123d78cdcec8ed8eef647391160
Jonathan Alvarado-Gomez <jonathan.alvaradogomez@revature.netgit config --global user.email jonathan.alvaradogomez@revature.netgit config --global user.email alvarado.jntn@gmail.comgit config --global user.name Jonathan>
2023-12-04 17:49:01 -0800

    updates to App and SnakeGame files.

--- src/main/java/org/alvarado/Snake/App.java
+++ src/main/java/org/alvarado/Snake/App.java
@@ -14,5 +14,9 @@ public class App {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        SnakeGame snakeGame = new SnakeGame(boardWidth, boardHeight);
+        frame.add(snakeGame);
+        frame.pack();
+
     }
 }
--- src/main/java/org/alvarado/Snake/SnakeGame.java
+++ src/main/java/org/alvarado/Snake/SnakeGame.java
@@ -7,16 +7,46 @@ import java.util.Random;
 import javax.swing.*;
 
 public class SnakeGame extends JPanel {
+    private class Tile {
+        int x;
+        int y;
+
+        Tile(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     int boardWidth;
     int boardHeight;
+    int tileSize = 25;
+    Tile snakeHead;
 
-    SnakeGame(int boardWidth, int boardHeight){
+    SnakeGame(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
 
         setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
         setBackground(Color.BLACK);
+
+        snakeHead = new Tile(5, 5);
     }
 
-// test comment Monday December 4, 2023.
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        //Grid Lines
+        for (int i = 0; i < boardWidth / tileSize; i++) {
+            g.drawLine(i * tileSize, 0, i * tileSize, boardHeight);
+            g.drawLine(0, i * tileSize, boardWidth, i * tileSize);
+        }
+
+
+        // Snake
+        g.setColor(Color.green);
+        g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize);
+    }
 }
