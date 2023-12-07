b8e4324efd7529361d5d6f32f8ce16fa3898343f
Jonathan Alvarado-Gomez <jonathan.alvaradogomez@revature.netgit config --global user.email jonathan.alvaradogomez@revature.netgit config --global user.email alvarado.jntn@gmail.comgit config --global user.name Jonathan>
2023-12-07 15:19:22 -0800

    Addings Game Loop. Adding random food location.

--- src/main/java/org/alvarado/Snake/SnakeGame.java
+++ src/main/java/org/alvarado/Snake/SnakeGame.java
@@ -6,7 +6,12 @@ import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
-public class SnakeGame extends JPanel {
+public class SnakeGame extends JPanel implements ActionListener{
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
+
     private class Tile {
         int x;
         int y;
@@ -24,7 +29,13 @@ public class SnakeGame extends JPanel {
     //Snake
     Tile snakeHead;
 
-    // you can resume working from here. Video was at minute 15.
+    // Food
+    Tile food;
+    Random random;
+
+
+    // Game Logic
+    Timer gameLoop;
 
     SnakeGame(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
@@ -34,6 +45,14 @@ public class SnakeGame extends JPanel {
         setBackground(Color.BLACK);
 
         snakeHead = new Tile(5, 5);
+
+        food = new Tile(10, 10);
+        random = new Random();
+        placeFood();
+
+        gameLoop = new Timer(100,this); // Delay is in milliseconds.
+        gameLoop.start();
+
     }
 
     public void paintComponent(Graphics g) {
@@ -49,8 +68,19 @@ public class SnakeGame extends JPanel {
         }
 
 
+        // Food
+        g.setColor(Color.red);
+        g.fillRect(food.x * tileSize, food.y * tileSize, tileSize, tileSize);
+
+
         // Snake
         g.setColor(Color.green);
         g.fillRect(snakeHead.x * tileSize, snakeHead.y * tileSize, tileSize, tileSize);
+    }
+
+    public void placeFood(){
+        // This will randomly place the coordinates of the food.
+        food.x = random.nextInt(boardWidth/tileSize); // 600/25 = 24
+        food.y = random.nextInt(boardHeight/tileSize); // 600/25 = 24
     }
 }
